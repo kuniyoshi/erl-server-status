1aed0ff65cc5832eff6f73351c63122e01c5c310
Kuniyoshi Kouji <kuniyoshi@cpan.com>
2013-09-19 22:08:22 +0900

    Change coding style.

--- src/server_status_app.erl
+++ src/server_status_app.erl
@@ -1,13 +1,6 @@
 -module(server_status_app).
-
 -behaviour(application).
-
-%% Application callbacks
 -export([start/2, stop/1]).
-
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
 
 start(_StartType, _StartArgs) ->
     server_status_sup:start_link().
--- src/server_status_worker.erl
+++ src/server_status_worker.erl
@@ -1,30 +1,16 @@
 -module(server_status_worker).
 -behaviour(gen_server).
--define(SERVER, ?MODULE).
-
-%% ------------------------------------------------------------------
-%% API Function Exports
-%% ------------------------------------------------------------------
-
 -export([start_link/0]).
-
-%% ------------------------------------------------------------------
-%% gen_server Function Exports
-%% ------------------------------------------------------------------
-
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
-
-%% ------------------------------------------------------------------
-%% API Function Definitions
-%% ------------------------------------------------------------------
+-export([init/1,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2,
+         code_change/3]).
+-define(SERVER, ?MODULE).
 
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
-
-%% ------------------------------------------------------------------
-%% gen_server Function Definitions
-%% ------------------------------------------------------------------
 
 init(Args) ->
     {ok, Args}.
@@ -43,8 +29,3 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
-
-%% ------------------------------------------------------------------
-%% Internal Function Definitions
-%% ------------------------------------------------------------------
-
